46b1e62f5f3370768f0e42794475a36705d09bd6
AravindKB <70912747+WindRmen05@users.noreply.github.com>
2022-01-21 13:41:37 +0530

    new update

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const {instrument} = require('@socket.io/admin-ui');
 const express = require("express");
-const { Socket } = require("socket.io-client");
+// const { Socket } = require("socket.io-client");
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
-  cors: { origin: ["http://localhost:3000","http://admin.socket.io/"] },
+  cors: { origin: ["http://192.168.88.77:3000","http://admin.socket.io/"] },
 });
 const port = 3000;
 
@@ -20,19 +20,25 @@ server.listen(port, () => {
 
 io.on("connect", (socket) => {
   console.log("User connected: " + socket.id);
-
-  socket.on("message", (message, room) => {
-    if (room === '') {
-      //message saved to db
-      socket.broadcast.emit("receive-message", message);
-    } else {
-      socket.broadcast.to(room).emit("receive-message", message);
-    }
-  });
-
-  socket.on('join-room', (room) => {
-    socket.join(room);
+  socket.on('/test', (data) => {
+    console.log(data);
   })
+
+  socket.emit('user_data', {name: "yatin", id: 2});
+  // socket.emit('new_user_data', {name: "yatin", id: "4"});
+
+  // socket.on("message", (message, room) => {
+  //   if (room === '') {
+  //     //message saved to db
+  //     socket.broadcast.emit("receive-message", message);
+  //   } else {
+  //     socket.broadcast.to(room).emit("receive-message", message);
+  //   }
+  // });
+
+  // socket.on('join-room', (room) => {
+  //   socket.join(room);
+  // })
 });
 
-instrument(io, {auth: false});
+// instrument(io, {auth: false});
